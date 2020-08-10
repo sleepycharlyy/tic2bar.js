7f1bb29d1cfb2dbdd6a295cd3ab9696334ab1741
Charly <sleepycharlyy@hotmail.com>
2020-08-10 18:40:48 +0200

    added qr code functionality

--- README.md
+++ README.md
@@ -2,34 +2,50 @@
 
 ## About
 
-This application encodes .tic cartridges for TIC-80 into barcodes for distribution  
+This command-line application encodes .tic cartridges for TIC-80 into barcodes or QR codes (default) for distribution  
 and is able to decode them back into .tic cartridges.  
 It uploads them to IPFS through the INFURA gateway  
-and stores the hash of the upload in barcodes.
-This project's made in node-js.  
+and stores the hash of the upload in the barcodes  
+or just the url of the cartridge in QR Codes.
+QR Codes can be decoded with any QR Scanner, but I've built one in this application for convenience's sake.  
 I took inspiration from [this](https://youtu.be/ExwqNreocpg) video from MattKC.
 
 [itch.io](https://sleepycharlyy.itch.io/tic2bar)
 
 ### Installation
 
-> npm install --global tic2bar
+`> npm install --global tic2bar`
 
 ### Usage
 
-> tic2bar
+`> tic2bar`
 
 ## Example
 
 ![Example](https://i.imgur.com/gubMXqE.png)
+![Example2](https://i.imgur.com/ebv7xJL.png)
+
+## Config
+
+You can change the encoding and decoding type in the config.json5 file.
+
+```js
+
+/* encoder & decoder type: qr or 128 (code128 barcode) */
+type: 'qr'
+
+```
 
 ## Dependencies
 
     - "canvas": "^2.6.1",
     - "ipfs-mini": "^1.1.5",
     - "jsbarcode": "^3.11.0",
+    - "jshint": "^2.12.0",
     - "json5": "^2.1.3",
+    - "node-qrdecode": "^1.0.3",
     - "prompt-sync": "^4.2.0",
+    - "qrcode": "^1.4.4",
     - "quagga": "^0.12.1"
 
 ## TIC-80
@@ -38,14 +54,17 @@ TIC-80 is a open source fantasy computer for making, playing and sharing little 
 [tic.computer](https://tic.computer/)  
 [nesbox/TIC-80](https://github.com/nesbox/TIC-80)  
 
-## License
-
-GPL-3.0 see LICENSE.md for details.
-
 ## Ending words
 
 The application is very bare bones and more of an idea.  
 The project is as it stands very gimmicky and not that useful,
-but i quite like the idea of storing TIC-80 cartridges in barcodes
+but i quite like the idea of storing TIC-80 cartridges in barcodes or QR codes
 for distribution and i can only imagine what kinds of stuff you could pull of,
 like scanning a barcode on mobile and playing it directly through some kind of app.
+
+## License
+
+GPL-3.0 see LICENSE.md for details.
+
+The word "QR Code" is registered trademark of:
+DENSO WAVE INCORPORATED
--- src/tic2bar.js
+++ src/tic2bar.js
@@ -3,20 +3,22 @@
 'use strict';
 
 /* Imports */
+const prompt = require('prompt-sync')(); /* terminal prompt functions */
 const fs = require('fs'); /* file system */
 const path = require('path'); /* path system */
+const QRCode = require('qrcode'); /* qr code encoder */
+const qrdecode = require('node-qrdecode'); /* qr code decoder */
 const JSON5 = require('json5') /* json5 parser */
 const jsbarcode = require('jsbarcode'); /* barcode creation */
 const Quagga = require('quagga').default; /* barcode scanning and decoding */
-const { createCanvas, loadImage, registerFont } = require('canvas'); /* canvas to draw barcode image */
-const prompt = require('prompt-sync')(); /* terminal prompt functions */
+const { createCanvas, registerFont } = require('canvas'); /* canvas to draw barcode image */
 const ipfs_mini = require('ipfs-mini'); /* ipfs api */
 const ipfs = new ipfs_mini({host: 'ipfs.infura.io', port: 5001, protocol: 'https'}) /* connecting to infura gateway */
 
 /* config (these variables will be overwritten by the config and are just default values) */
 var debug_mode = false; /* debug mode */
 var timeout_countdown = 30000; /* how long in milisec till timeout error is thrown */
-var type = 'qr'; /* encoder and decoder type qr or bar */
+var type = 'qr'; /* encoder and decoder type qr or 128 (for code128 barcode) */
 
 /* log function to log messages to the console */ 
 function log(msg, type){
@@ -37,55 +39,9 @@ function log(msg, type){
     }
 }
 
-/* create barcode and save it. takes (ipfs) hash and file_path (to save png to) and the game_title as inputs */
-function bar_create(hash, file_path, game_title){
-    registerFont('assets/tic-80-wide-font.ttf', { family: 'Tic' });
-
-    /* create canvas */
-    var canvas = createCanvas(200, 200);
-
-    /* create bar code */
-    log('Creating barcode', 'info');
-    jsbarcode(canvas, hash, {
-        font: 'Tic',
-        fontSize: 16,
-        textMargin: 10,
-        height: 200,
-        width: 1,
-        margin: 30,
-        marginTop: 80,
-        background: '#deeed6',
-        lineColor: '#140c1c',
-        text: game_title.toLowerCase() + ' - tic-80 cartridge'
-    });
-
-    /* save file as jpg or png*/
-    const out = fs.createWriteStream(file_path)
-    if (file_path.split('.').pop() == 'jpg'){
-        const stream = canvas.createJPEGStream()
-        stream.pipe(out)
-        out.on('finish', () => {
-            /* finished */
-            log('Barcode was created and saved to: ' + file_path, 'success'); 
-            log('Finished! ʕ•ᴥ•ʔ', 'success');
-            process.exit(1);
-        })
-    }else{
-        const stream = canvas.createPNGStream()
-        stream.pipe(out)
-        out.on('finish', () => {
-            /* finished */
-            log('Barcode was created and saved to: ' + file_path, 'success'); 
-            log('Finished! ʕ•ᴥ•ʔ', 'success');
-            process.exit(1);
-        })
-    }
- 
-
-}
 
 /* create .tic file.. takes in data and file_path to save the .tic to */
-function create_tic(data, file_path){
+function tic_create(data, file_path){
     log('Writing data', 'info');
 
     fs.writeFileSync(file_path, data, 'binary');
@@ -96,7 +52,7 @@ function create_tic(data, file_path){
 
 }
 
-/* encode file (expected .tic, and game_title) */
+/* encode file to barcode (expected input_path (.tic), output_path and game_title) */
 function bar_encode(input_path, output_path, game_title){ 
     /* load file */
     log('Loading:  ' + input_path, 'info');
@@ -123,22 +79,63 @@ function bar_encode(input_path, output_path, game_title){
     setInterval(function(){
         if(hash != ''){
             /* hash data has arrived => create bar code and stop timers */
-            bar_create(hash, output_path, game_title);
             clearTimeout(this);
             clearTimeout(timeout_timer);
+
+            registerFont('assets/tic-80-wide-font.ttf', { family: 'Tic' });
+
+            /* create canvas */
+            var canvas = createCanvas(200, 200);
+        
+            /* create bar code */
+            log('Creating barcode', 'info');
+            jsbarcode(canvas, hash, {
+                font: 'Tic',
+                fontSize: 16,
+                textMargin: 10,
+                height: 200,
+                width: 1,
+                margin: 30,
+                marginTop: 80,
+                background: '#deeed6',
+                lineColor: '#140c1c',
+                text: input_path.split('/').pop().toLowerCase() + ' - tic-80 cartridge'
+            });
+        
+            /* save file as jpg or png*/
+            const out = fs.createWriteStream(output_path)
+            if (output_path.split('.').pop() == 'jpg'){
+                const stream = canvas.createJPEGStream()
+                stream.pipe(out)
+                out.on('finish', () => {
+                    /* finished */
+                    log('Barcode was created and saved to: ' + output_path, 'success'); 
+                    log('Finished! ʕ•ᴥ•ʔ', 'success');
+                    process.exit(1);
+                })
+            }else{
+                const stream = canvas.createPNGStream()
+                stream.pipe(out)
+                out.on('finish', () => {
+                    /* finished */
+                    log('Barcode was created and saved to: ' + output_path, 'success'); 
+                    log('Finished! ʕ•ᴥ•ʔ', 'success');
+                    process.exit(1);
+                })
+            }
         }
     }, 1000)
 }
 
 /* decode barcode (expected input_path to .png or .jpg, output_path to .tic and game_title) */
-function bar_decode(input_path, output_path, game_title){
+function bar_decode(input_path, output_path){
     /* decoding barcode */
     log('Starting to decode barcode', 'info');
     Quagga.decodeSingle({
         src: input_path,
         numOfWorkers: 0,  /* Needs to be 0 when used within node */
         inputStream: {
-            size: 1000  /* restrict input-size to be 1000px in width (long-side) */
+            size: 2000  /* restrict input-size to be 2000px in width (long-side) */
         },
         decoder: {
             readers: ["code_128_reader"] /* list of active readers */
@@ -153,8 +150,8 @@ function bar_decode(input_path, output_path, game_title){
             /* download .zip from ipfs */
             log('Downloading cartridge data from IPFS Gateway', 'info');
             
-            if(hash == undefined){
-                log('The hash code is undefined. Probably a timeout error. Check your connection!', 'error');
+            if(hash == null){
+                log('Data is corrupted. Check your barcode!', 'error');
             }
             
             let data = '';
@@ -178,7 +175,7 @@ function bar_decode(input_path, output_path, game_title){
             setInterval(function(){
                 if(data != ''){
                     /* zip data has arrived => create .tic and stop timers */
-                    create_tic(data, output_path, game_title);
+                    tic_create(data, output_path);
                     clearTimeout(this);
                     clearTimeout(timeout_timer);
                 }
@@ -186,6 +183,120 @@ function bar_decode(input_path, output_path, game_title){
         } else {
             log("Didn't detect a barcode. Check your input file!", 'error');
         }
+    });
+}
+
+/* encode file to qr code (expected input_path (.tic), output_path) */
+function qr_encode(input_path, output_path){ 
+    /* load file */
+    log('Loading:  ' + input_path, 'info');
+    let file = fs.readFileSync(input_path, 'binary');
+
+    /* ipfs upload */
+    log('Uploading cartridge to IPFS Gateway', 'info');
+    let hash = '';
+    ipfs.add(file, async (error, hash_) => {
+        if (error) {
+            return log('Upload to IPFS failed (' + error + ')', 'error');
+        }
+        hash = hash_;
+        log('Successfully uploaded cartridge to: ' + 'https://ipfs.infura.io/ipfs/' + hash_, 'success');
+
+    })
+    
+    /* start connection timeout timer */
+    let timeout_timer = setTimeout(function(){
+        /* connection timed out */
+        log('Connection timeout. Check your internet connection or try again later!', 'error');
+    }, timeout_countdown)
+    /* check if hash data has arrived every second */
+    setInterval(function(){
+        if(hash != ''){
+            /* hash data has arrived => create qr code and stop timers */
+            clearTimeout(this);
+            clearTimeout(timeout_timer);
+
+            /* create canvas */
+            var canvas = createCanvas(300, 300);
+        
+            /* create qr code */
+            log('Creating QR code', 'info');
+            QRCode.toCanvas(canvas, 'https://ipfs.infura.io/ipfs/' + hash, { errorCorrectionLevel: 'H' }, function (error) {
+                if (error) log(error, 'error');
+                log('Successfully encoded QR Code!','success');
+
+                /* save file as jpg or png*/
+                const out = fs.createWriteStream(output_path);
+                if (output_path.split('.').pop() == 'jpg'){
+                    const stream = canvas.createJPEGStream();
+                    stream.pipe(out)
+                    out.on('finish', () => {
+                        /* finished */
+                        log('QR Code was created and saved to: ' + output_path, 'success'); 
+                        log('Finished! ʕ•ᴥ•ʔ', 'success');
+                        process.exit(1);
+                    })
+                }else{
+                    const stream = canvas.createPNGStream()
+                    stream.pipe(out)
+                    out.on('finish', () => {
+                        /* finished */
+                        log('QR Code was created and saved to: ' + output_path, 'success'); 
+                        log('Finished! ʕ•ᴥ•ʔ', 'success');
+                        process.exit(1);
+                    })
+                }
+                })
+        }
+    }, 1000)
+}
+
+/* decode qr code (expected input_path to .png or .jpg, output_path to .tic) */
+function qr_decode(input_path, output_path){
+    /* decoding qr code */
+    log('Starting to decode QR code', 'info');
+    qrdecode(input_path).then(function(url){
+        if(url == null){
+            log('Data is corrupted. Check your QR Code!', 'error');
+        }
+
+        log(url, 'debug');
+
+        /* success of decoding */
+        log('\n');
+        log('Successfully decoded QR code! url: ' + url, 'success');
+
+        /* download .tic from ipfs */
+        log('Downloading cartridge data from IPFS Gateway', 'info');
+         
+
+         
+         let data_ = '';
+         ipfs.cat(url.split('/').pop(), (error, result) => {
+             if (error) {
+                 log('There was an error while downloading from IPFS: ' + error, 'error');
+             } else {
+                /* success of downloading */
+                log('Successfully downloaded cartridge data!', 'success');
+                data_ = result;
+            }
+        });
+
+
+         /* start connection timeout timer */
+         let timeout_timer = setTimeout(function(){
+             /* connection timed out */
+             log('Connection timeout. Check your internet connection or try again later!', 'error');
+         }, timeout_countdown)
+         /* check if zip data has arrived every second */
+         setInterval(function(){
+             if(data_ != ''){
+                 /* zip data has arrived => create .tic and stop timers */
+                 tic_create(data_, output_path);
+                 clearTimeout(this);
+                 clearTimeout(timeout_timer);
+             }
+         }, 1000)
     });
 }
 
@@ -200,18 +311,16 @@ function load_config(){
     timeout_countdown = config.timeout_countdown;
     type = config.type;
 
-    /* debug */
-    log('Settings:', 'debug');
+    /* log config settings */
     log('debug_mode: ' + debug_mode, 'debug');
-    log('type: ' + type, 'debug');
+    log('encoding / decoding type: ' + type, 'info');
     log('timeout_countdown: ' + timeout_countdown, 'debug');
     log('\n');
 }
 
-
+/* main function (gets run when application starts) */
 function main(){
     /* declare variables */
-    let game_title = '';
     let input_path = '';
     let output_path = '';
 
@@ -222,18 +331,16 @@ function main(){
 
     /* main loop */
     while(true){
-        switch(prompt('Do you want to encode a .tic cartridge or decode a barcode? (e / d) >> ')) {
+        switch(prompt('Do you want to encode or decode? (e / d) >> ').toLowerCase()) {
         case 'd': 
             log('\x1b[36m' + '\nYou selected decode ');
 
-            game_title = prompt('What is the title of the game you want to decode? >> ');
-
-            /* check if game_title is empty */
-            if (game_title == undefined || game_title == " " || game_title == null) {
-                log("The game title can't be empty!", 'error');
+            /* check if bar code or qr code */
+            if(type == '128'){
+                input_path = prompt('What is the path to the barcode you want to decode? (.png or .jpg) >> '); 
+            } else {
+                input_path = prompt('What is the path to the QR code you want to decode? (.png or .jpg) >> '); 
             }
-
-            input_path = prompt('What is the path to the barcode you want to decode? (.png or .jpg) >> ');   
             
             /* check if file path is empty */
             if (input_path == undefined || input_path == " " || input_path == null){
@@ -246,6 +353,7 @@ function main(){
             }
             /* check if file exists */
             if(!fs.existsSync(input_path)) {
+                log(input_path);
                 log("This file doesn't exist!", 'error');
             }
             
@@ -263,17 +371,15 @@ function main(){
 
             /* decode file */
             log('\n');
-            bar_decode(input_path, output_path, game_title)
+            /* check if bar code or qr code */
+            if(type == '128'){
+                bar_decode(input_path, output_path);
+            }else{
+                qr_decode(input_path, output_path);
+            }
             break;
         case 'e': 
             log('\x1b[36m' + '\nYou selected encode ');
-
-            game_title = prompt('What is the title of the game you want to encode? >> ');
-
-            /* check if game_title is empty */
-            if (game_title == undefined || game_title == " " || game_title == null) {
-                log("The game title can't be empty!", 'error');
-            }
 
             input_path = prompt('What is the path to the .tic game cartridge you want to encode? >> ');   
 
@@ -287,11 +393,16 @@ function main(){
                 log('This filetype is unrecognized, use .tic files!', 'error');
             }
             /* check if file exists */
-            if(!fs.existsSync(input_path, input_path)) {
+            if(!fs.existsSync(input_path)) {
                 log("This file doesn't exist!", 'error');
             }
 
-            output_path = prompt('Where do you want to export the barcode to? (.png or .jpg) >> ');   
+            /* check if bar code or qr code */
+            if(type == '128'){
+                output_path = prompt('Where do you want to export the barcode to? (.png or .jpg) >> ');  
+            } else {
+                output_path = prompt('Where do you want to export the QR code to? (.png or .jpg) >> ');  
+            }
             
             /* check if file path is empty */
             if (output_path == undefined || output_path == " " || output_path == null){
@@ -305,14 +416,20 @@ function main(){
 
             /* encode file */
             log('\n');
-            bar_encode(input_path, output_path, game_title);
+            /* check if bar code or qr code */
+            if(type == '128'){
+                bar_encode(input_path, output_path);
+            }else{
+                qr_encode(input_path, output_path);
+            }
             break;
         default:
-            /* when the user didn't input e or d the whole prompt repeats */
+            /* when the user didn't input 'e' or 'd' the whole prompt repeats */
             continue;
         }
         break;
     }
 }
 
-main()
+main();
+
